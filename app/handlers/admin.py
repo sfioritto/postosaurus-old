c4c74bde96abe7518d93441b5e4dd4c7b296e531
Sean Fioritto <sean@zeus.(none)>
2009-10-25 14:35:23 -0500

    Messages posted by a user are no longer sent back to that user.

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -9,7 +9,7 @@ from app.model import mailinglist
 @route('(list_name)@(host)')
 def POSTING(message, list_name=None, host=None):
     if mailinglist.is_subscribed(message['from'], list_name):
-        mailinglist.post_message(relay, message, list_name, host)
+        mailinglist.post_message(relay, message, list_name, host, message['from'])
     return POSTING
 
 
--- tests/handlers/admin_tests.py
+++ tests/handlers/admin_tests.py
@@ -6,6 +6,7 @@ from nose import with_setup
 
 relay = relay(port=8823)
 sender = "sender@sender.com"
+member = "member@member.com"
 host = "postosaurus.com"
 list_name = "test.list"
 list_addr = "%s@%s" % (list_name, host)
@@ -17,10 +18,12 @@ def setup_func():
     mlist = MailingList(name = list_name, email = list_addr)
     mlist.save()
 
+
 def teardown_func():
     MailingList.objects.all().delete()
     Subscription.objects.all().delete()
     User.objects.all().delete()    
+
 
 def subscribe_user(address):
     mlist = MailingList.objects.filter(email = list_addr)[0]
@@ -34,13 +37,16 @@ def subscribe_user(address):
 def test_existing_user_posts_message():
 
     """
-    Moves the user to the POSTING state and posts a message.
+    Posts a message and checks to see that the one member
+    was sent a message.
     """
     
     subscribe_user(sender)
-    msg = client.say(list_addr, "My first message.",
-                     list_addr)
-    assert delivered(list_addr)
+    subscribe_user(member)
+    msg = client.say(list_addr, "My first message.")
+
+    assert queue().count() == 1
+
 
 @with_setup(setup_func, teardown_func)
 def test_non_user_posts_message():
@@ -63,9 +69,9 @@ def test_forwards_to_posting():
     """
     subscribe_user(sender)
     client.begin()
-    client.say(list_addr, "Test that forward works.", list_addr)
+    client.say(list_addr, "Test that forward works.")
     assert_in_state('app.handlers.admin', list_addr, sender, 'POSTING')
-    assert delivered(list_addr)
+
 
 
 
